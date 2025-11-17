0adaf4b3163fcab1013c873fa6c1ce92a4bfb189
SIVAKUMAR S <122508923+SIVA2118@users.noreply.github.com>
2025-11-17 12:42:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://sivakumar:siva@cluster0.ufpyq.mongodb.net/boutique")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("You! Connected to MongoDB..."))
   .catch((err) =>
     console.error("Could not connect to MongoDB... " + err.message)
